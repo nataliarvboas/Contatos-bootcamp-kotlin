8d1278bfac6bfa36d0230cb59944f1180bafa100
nataliarvboas <nataliarvboas@gmail.com>
2021-04-05 11:33:47 -0300

    implement adapter

--- app/src/main/java/com/example/contatos_bootcamp_kotlin/ContactAdapter.kt
+++ app/src/main/java/com/example/contatos_bootcamp_kotlin/ContactAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.contatos_bootcamp_kotlin
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
@@ -14,4 +17,15 @@ class ContactAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapter
     }
 
     override fun getItemCount() = contactsList.size
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bindItem(contact: Contact) {
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.contact_phone)
+
+            textName.text = contact.name
+            textPhone.text = contact.phone
+        }
+
+    }
 }
